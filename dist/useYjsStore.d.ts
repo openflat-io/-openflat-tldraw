c41487b84fdf03b37c0f29fb49cdc64913ca9f62
qianzhu <36325249+syt-honey@users.noreply.github.com>
2024-01-24 16:35:31 +0800

    update pkg name

--- dist/useYjsStore.d.ts
+++
@@ -1,8 +0,0 @@
-import { TLAnyShapeUtilConstructor, TLRecord, StoreSnapshot, TLStoreWithStatus } from "@tldraw/tldraw";
-export declare function useYjsStore({ roomId, hostUrl, shapeUtils, defaultStore, }: Partial<{
-    hostUrl: string;
-    roomId: string;
-    version: number;
-    shapeUtils: TLAnyShapeUtilConstructor[];
-    defaultStore: StoreSnapshot<TLRecord>;
-}>): TLStoreWithStatus;
